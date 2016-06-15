3cc3c562da5fa52fc2c0525c2eaf57cccc004785
Krzysztof Młynarski <kmlynarski@wi.zut.edu.pl>
2016-06-15 11:56:47 +0200

    Fix problem with display parcel list and sender list

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 /* Modules */
-var app = angular.module('postmanPanel', ['taskControllers',
+var app = angular.module('postmanPanel', ['taskControllers', 'parcelControllers', 'parcelServices', 'senderControllers', 'senderServices',
 										  'taskServices',
 										  'dashboardControllers',
 										  'userControllers',
@@ -13,18 +13,28 @@ var app = angular.module('postmanPanel', ['taskControllers',
 										  ]);
 
 app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login',{
-      templateUrl: '/bundles/app/partials/user/login-form.html',
-      controller: 'LoginUserForm'
+    $routeProvider.
+	when('/login',{
+		templateUrl: '/bundles/app/partials/user/login-form.html',
+		controller: 'LoginUserForm'
     }).
 	when('/postman/new', {
-      templateUrl: '/bundles/app/partials/postman/postman-form.html',
-      controller: 'CreatePostmanForm'
-  }).
+		templateUrl: '/bundles/app/partials/postman/postman-form.html',
+		controller: 'CreatePostmanForm'
+	}).
 	when('/tasklist',{
 			templateUrl: '/bundles/app/partials/task-list.html',
 			controller: 'TaskListCtrl'
-	}).otherwise({
+	}).
+	when('/senderlist',{
+		templateUrl: '/bundles/app/partials/sender-list.html',
+		controller: 'SenderListCtrl'
+	}).
+	when('/parcellist',{
+			templateUrl: '/bundles/app/partials/parcel-list.html',
+			controller: 'ParcelListCtrl'
+	}).
+	otherwise({
       templateUrl: '/bundles/app/partials/index.html',
       controller: 'Dashboard'
     });;
